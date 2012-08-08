49f1e96a01c80a33c5ae6c21e903831ac610edf6
bogdan <bogdan@itechcraft.com>
2012-08-08 13:19:19 +0300

    YII_DEBUG string with IP can be duplicated with different IP

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // Debug is on when remote address is localhost
-defined('YII_DEBUG') or define('YII_DEBUG', $_SERVER['REMOTE_ADDR'] === '127.0.0.1');
+defined('YII_DEBUG') or $_SERVER['REMOTE_ADDR'] === '127.0.0.1' and define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 ini_set('display_errors',         YII_DEBUG ? 1 : 0);
 ini_set('display_startup_errors', YII_DEBUG ? 1 : 0);
 error_reporting(YII_DEBUG ? E_ALL : 0);
