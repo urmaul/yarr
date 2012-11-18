7dbecd4ccd31dd186a78e8f2b30b1d155680bb00
Bogdan AspireOne <blacksilver@codesex.org>
2012-11-18 23:53:09 +0200

    Homepage route added

--- protected/config/main.php
+++ protected/config/main.php
@@ -31,6 +31,7 @@ $config = array(
 			'showScriptName' => false,
 			'urlFormat' => 'path',
 			'rules'=>array(
+				'' => 'site/index',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
