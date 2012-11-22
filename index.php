bf2010c8e0c7bc9468a99d5e5c949cd2e3e9ce08
urmaul <bogdan@itechcraft.com>
2012-11-22 10:05:00 +0200

    space indentation

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ ini_set('display_startup_errors', YII_DEBUG ? 1 : 0);
 error_reporting(YII_DEBUG ? E_ALL : 0);
 
 // change the following paths if necessary
-$yii=dirname(__FILE__) . '/framework/' . (YII_DEBUG ? 'yii.php' : 'yiilite.php');
-$config=dirname(__FILE__).'/protected/config/main.php';
+$yii    = dirname(__FILE__) . '/framework/' . (YII_DEBUG ? 'yii.php' : 'yiilite.php');
+$config = dirname(__FILE__) . '/protected/config/main.php';
 
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
