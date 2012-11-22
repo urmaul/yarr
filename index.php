8799ea8f6d6c40b8dd0ddd2b48f8e392e996f295
urmaul <bogdan@itechcraft.com>
2012-11-22 10:03:47 +0200

    Including yiilite.php when YII_DEBUG is off

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
 // Debug is on when remote address is localhost
 defined('YII_DEBUG') or $_SERVER['REMOTE_ADDR'] === '127.0.0.1' and define('YII_DEBUG', true);
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 ini_set('display_errors',         YII_DEBUG ? 1 : 0);
 ini_set('display_startup_errors', YII_DEBUG ? 1 : 0);
 error_reporting(YII_DEBUG ? E_ALL : 0);
+
+// change the following paths if necessary
+$yii=dirname(__FILE__) . '/framework/' . (YII_DEBUG ? 'yii.php' : 'yiilite.php');
+$config=dirname(__FILE__).'/protected/config/main.php';
 
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
