1036247031b59db5f7d5151fae620acba8f33a80
urmaul <bogdan@itechcraft.com>
2012-12-21 13:39:38 +0200

    column2f layout one more fix

--- protected/views/layouts/column2f.php
+++ protected/views/layouts/column2f.php
@@ -1,7 +1,7 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
 <div id="content">
-    <div id="right sidebar">
+    <div id="sidebar" class="right span-5">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
 			'title'=>'Operations',
